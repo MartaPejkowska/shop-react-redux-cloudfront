c9755a8b7e699c35a95e210d5ffd489f8b81a8c3
MartaPejkowska <marta.pejkowska@coderscrew.pl>
2023-06-18 10:17:15 +0200

    feat: add import link

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://fnvhngivp1.execute-api.eu-west-1.amazonaws.com",
   bff: "https://yhubbm0idb.execute-api.eu-west-1.amazonaws.com",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
